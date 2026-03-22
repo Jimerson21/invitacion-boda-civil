a2eb1488778529cf5b8e7d32cc65d71494428664
Jimerson Montiel <jnmpmontiel@gmail.com>
2026-03-22 13:55:18 -0400

    Update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/invitation-boda-civil/",
+  base: "/invitacion-boda-civil/",
   server: {
     host: "::",
     port: 8080,
